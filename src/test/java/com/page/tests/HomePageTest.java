61e120c469e7654dac24bd88e902e87eba8d7a3d
Arvind <arvind235@gmail.com>
2015-09-09 19:49:28 +1000

    Second commit

---
+++ src/test/java/com/page/tests/HomePageTest.java
@@ -0,0 +1,46 @@
+package com.page.tests;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class HomePageTest {
+	
+	WebDriver driver;
+	@BeforeTest
+	public void beforeTest(){
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+		driver.get("http://newtours.demoaut.com/");
+	}	
+	@Test
+	public void Login(){
+		driver.findElement(By.name("userName")).sendKeys("user1@gmail.com");;
+		driver.findElement(By.name("password")).sendKeys("user1");
+		driver.findElement(By.name("login")).click();	
+		System.out.println(driver.getTitle());
+		
+		driver.findElement(By.linkText("Flights")).click();		
+		System.out.println(driver.getTitle());
+		
+		driver.findElement(By.linkText("Hotels")).click();		
+		System.out.println(driver.getTitle());
+		
+	}	
+	/*@Test
+	public void FlightBooking(){
+		driver.findElement(By.linkText("Flights")).click();		
+		System.out.println(driver.getTitle());
+	}*/	
+	@AfterTest
+	public void afterTest(){
+		driver.quit();
+	}
+}
